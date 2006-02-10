2e5faaa21b19ccda47cf18bb6d5c7b441a0694f0
Chris Porter <slug@quakenet.org>
2006-02-10 21:19:00 +0000

    Cleanup of noperserv_commands. Removed opchan.

--- noperserv/noperserv_commands.c
+++ noperserv/noperserv_commands.c
@@ -13,128 +13,56 @@ int controlopchan(void *sender, int cargc, char **cargv);
 int controlkick(void *sender, int cargc, char **cargv);
 
 void _init() {
-  registercontrolhelpcmd("kill",NO_OPER,2,&controlkill,"Usage: kill nick <reason>\nKill specificed user.");
-  registercontrolhelpcmd("opchan",NO_OPER,2,&controlopchan,"Usage: opchan channel nick\nGive user +o on channel.");
-  registercontrolhelpcmd("kick",NO_OPER,3,&controlkick,"Usage: kick channel user <reason>\nKick a user from the given channel");
+  registercontrolhelpcmd("kill", NO_OPER, 2, &controlkill, "Usage: kill <nick> ?reason?\nKill specified user with given reason (or 'Killed').");
+  registercontrolhelpcmd("kick", NO_OPER, 3, &controlkick, "Usage: kick <channel> <user> ?reason?\nKick a user from the given channel, with given reason (or 'Kicked').");
 }
 
 void _fini() {
-  deregistercontrolcmd("kill",controlkill); 
-  deregistercontrolcmd("opchan",controlopchan);
-  deregistercontrolcmd("kick",controlkick);
+  deregistercontrolcmd("kill", controlkill); 
+  deregistercontrolcmd("kick", controlkick);
 }
 
 int controlkick(void *sender, int cargc, char **cargv) {
   nick *np=(nick *)sender;
-  nick *victim;
   channel *cp;
-  modechanges changes;
   nick *target;
 
-  if (cargc<2) {
-    controlreply(sender,"Usage: kick channel user <reason>");
+  if (cargc<2)
+    return CMD_USAGE;
+
+  if ((cp=findchannel(cargv[0]))==NULL) {
+    controlreply(np,"Couldn't find that channel.");
     return CMD_ERROR;
   }
 
-  if ((cp=findchannel(cargv[0]))!=NULL) {
-    if (cargv[1][0]=='#') {
-      if (!(target=getnickbynumericstr(cargv[1]+1))) {
-        controlreply(np,"Sorry, couldn't find numeric %s",cargv[0]+1);
-        return CMD_ERROR;
-      }
-    } else {
-      if ((target=getnickbynick(cargv[1]))==NULL) {
-        controlreply(np,"Sorry, couldn't find that user");
-        return CMD_ERROR;
-      }
-    }
-
-    if(cargc > 2) {
-      irc_send("%s K %s %s :%s",mynumeric->content,cp->index->name->content,longtonumeric(target->numeric,5),cargv[2]);
-    } else {
-      irc_send("%s K %s %s :Kicked",mynumeric->content,cp->index->name->content,longtonumeric(target->numeric,5));
-    }
-    delnickfromchannel(cp, target->numeric, 1);
-
-    controlreply(sender,"Put Kick for %s from %s.", target->nick, cp->index->name->content);
-    controlwall(NO_OPER, NL_KICKS, "%s/%s sent KICK for %s!%s@%s from %s", np->nick, np->authname, target->nick, target->ident, target->host->name->content,cp->index->name->content);
-
-  } else {
-    controlreply(np,"Couldn't find channel %s.",cargv[0]);
-    return;
+  if ((target=getnickbynick(cargv[1]))==NULL) {
+    controlreply(np,"Sorry, couldn't find that user");
+    return CMD_ERROR;
   }
+
+  controlwall(NO_OPER, NL_KICKS, "%s/%s sent KICK for %s!%s@%s on %s (%s)", np->nick, np->authname, target->nick, target->ident, target->host->name->content,cp->index->name->content, (cargc>2)?cargv[2]:"Kicked");
+  localkickuser(NULL, cp, target, (cargc>2)?cargv[2]:"Kicked");
+  controlreply(sender, "KICK sent.");
 
   return CMD_OK;
 }
 
-int controlopchan(void *source, int cargc, char **cargv) {
-  nick *sender=(nick *)source;
-  nick *victim;
-  channel *cp;
-  modechanges changes;
-  nick *target;
-  unsigned long *lp;
-
-  if (cargc<2) {
-    controlreply(sender,"Usage: opchan channel user");
-    return CMD_ERROR;
-  }
-  
-  if ((cp=findchannel(cargv[0]))!=NULL) {
-    if (cargv[1][0]=='#') {
-      if (!(target=getnickbynumericstr(cargv[1]+1))) {
-        controlreply(sender,"Sorry, couldn't find numeric %s",cargv[0]+1);
-        return CMD_ERROR;
-      }
-    } else {
-      if ((target=getnickbynick(cargv[1]))==NULL) {
-        controlreply((nick *)sender,"Sorry, couldn't find that user");
-        return CMD_ERROR;
-      }
-    }
-
-    if ((lp=getnumerichandlefromchanhash(cp->users,target->numeric))==NULL) {
-      controlreply((nick *)sender,"Sorry, User not on channel");
-      return CMD_ERROR;
-    }
-
-    (*lp)|=CUMODE_OP;
-    irc_send("%s OM %s +o %s",mynumeric->content,cp->index->name->content,longtonumeric(target->numeric,5));
-    controlreply(sender,"Put mode +o %s on %s.", target->nick, cp->index->name->content);  
-  } else {
-    controlreply(sender,"Couldn't find channel %s.",cargv[0]);
-    return;
-  }
-  
-  return CMD_OK;
-}
-
+/* NO checking to see if it's us or anyone important */
 int controlkill(void *sender, int cargc, char **cargv) {
   nick *target;
-  char buf[BUFSIZE];
-  int i;
   nick *np = (nick *)sender;
  
-  if (cargc<1) {
-    controlreply(np,"Usage: kill <user> <reason>");
+  if (cargc<1)
+    return CMD_USAGE;  
+
+  if ((target=getnickbynick(cargv[0]))==NULL) {
+    controlreply(np,"Sorry, couldn't find that user.");
     return CMD_ERROR;
   }
-  
-  if (cargv[0][0]=='#') {
-    if (!(target=getnickbynumericstr(cargv[0]+1))) {
-      controlreply(np,"Sorry, couldn't find numeric %s",cargv[0]+1);
-      return CMD_ERROR;
-    }
-  } else {
-    if ((target=getnickbynick(cargv[0]))==NULL) {
-      controlreply(np,"Sorry, couldn't find that user");
-      return CMD_ERROR;
-    }
-  }
 
+  controlwall(NO_OPER, NL_KILLS, "%s/%s sent KILL for %s!%s@%s (%s)", np->nick, np->authname, target->nick, target->ident, target->host->name->content, (cargc>1)?cargv[1]:"Killed");
   killuser(NULL, target, (cargc>1)?cargv[1]:"Killed");
-  controlreply(np,"KILL sent.");
-  controlwall(NO_OPER, NL_KILLS, "%s/%s sent KILL for %s!%s@%s (%s)", np->nick, np->authname, target->nick, target->ident, target->host->name->content,(cargc>1)?cargv[1]:"Killed");
+  controlreply(np, "KILL sent.");
 
   return CMD_OK;
 }
