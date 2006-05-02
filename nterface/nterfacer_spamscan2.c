59508d2086e1931f46e92a7a1efbec7135406a03
Chris Porter <slug@quakenet.org>
2006-05-02 20:48:00 +0100

    Add addchan to nterfacer_spamscan2

--- nterface/nterfacer_spamscan2.c
+++ nterface/nterfacer_spamscan2.c
@@ -16,10 +16,13 @@
 #define ERR_CHANNEL_NOT_REGISTERED      0x02
 #define ERR_PROFILE_DOES_NOT_EXIST      0x03
 #define ERR_BAD_FLAGS                   0x04
+#define ERR_MEMORY_ERROR                0x05
+#define ERR_CHANNEL_ALREADY_REGISTERED  0x06
 
 int handle_getprofile(struct rline *li, int argc, char **argv);
 int handle_setprofile(struct rline *li, int argc, char **argv);
 int handle_delchan(struct rline *li, int argc, char **argv);
+int handle_addchan(struct rline *li, int argc, char **argv);
 
 struct service_node *s_node;
 
@@ -31,6 +34,7 @@ void _init(void) {
   register_handler(s_node, "getprofile", 1, handle_getprofile);
   register_handler(s_node, "setprofile", 3, handle_setprofile);
   register_handler(s_node, "delchan", 1, handle_delchan);
+  register_handler(s_node, "addchan", 2, handle_addchan);
 }
 
 void _fini(void) {
@@ -88,6 +92,53 @@ int handle_delchan(struct rline *li, int argc, char **argv) {
 
   if(spamscan_deletechannelsettings(argv[0]))
     return ri_error(li, ERR_CHANNEL_NOT_REGISTERED, "Channel not registered");
+
+  ri_append(li, "Done");
+  return ri_final(li);
+}
+
+int handle_addchan(struct rline *li, int argc, char **argv) {
+  spamscan_channelsettings *cs;
+  spamscan_channelprofile *cp;
+  flag_t flags = 0;
+  chanindex *sc_index;
+
+  if(argv[0][0] != '#')
+    return ri_error(li, ERR_TARGET_NOT_FOUND, "Channel not found");
+
+  if(spamscan_getchannelsettings(argv[0], 0))
+    return ri_error(li, ERR_CHANNEL_ALREADY_REGISTERED, "Channel already registered");
+
+  if((argc > 2) && (setflags(&flags, SPAMSCAN_CF_ALL, argv[2], s_cfflags, REJECT_UNKNOWN) != REJECT_NONE))
+    return ri_error(li, ERR_BAD_FLAGS, "Bad flags");
+
+  cp = spamscan_getchannelprofile(argv[1], 0);
+  if(!cp)
+    return ri_error(li, ERR_PROFILE_DOES_NOT_EXIST, "Profile does not exist");
+
+  cs = spamscan_getchannelsettings(argv[0], 1);
+  if(!cs)
+    return ri_error(li, ERR_MEMORY_ERROR, "Memory allocation error");
+
+  cs->cp = cp;
+  cs->flags = flags;
+  cs->modified = time(NULL);
+  cs->modifiedby = spamscan_getaccountsettings("nterfacer", 0); /* ugly hack, should be 1... */
+
+  sc_index = findorcreatechanindex(argv[0]);
+  if(sc_index) {
+    spamscan_channelext *ce = spamscan_getchanext(sc_index, 1);
+    ce->cs = cs;
+
+    if (s_nickname && !CFIsSuspended(cs) && sc_index->channel) {
+      ce->joinedchan = 1;
+      localjoinchannel(s_nickname, sc_index->channel);
+      localgetops(s_nickname, sc_index->channel);
+    }    
+  }
+
+  spamscan_insertchanneldb(cs);
+  spamscan_log("ADDCHAN: %s added %s with flags %s", "nterfacer", argv[0], printflags(cs->flags, s_cfflags));
 
   ri_append(li, "Done");
   return ri_final(li);
