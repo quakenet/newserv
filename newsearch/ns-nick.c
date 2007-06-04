3adfb300cb8992c3d3747aad33570df0b3b44d79
Ian Barker <brks@quakenet.org>
2007-06-04 19:31:19 +0100

    modified ns-nick.c to now work in both nicksearch and chansearch

--- newsearch/ns-nick.c
+++ newsearch/ns-nick.c
@@ -7,24 +7,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct nick_localdata {
+  nick *np;
+  int type;
+};
+
 void *nick_exe(struct searchNode *thenode, int type, void *theinput);
 void nick_free(struct searchNode *thenode);
 
 struct searchNode *nick_parse(int type, int argc, char **argv) {
+  struct nick_localdata *localdata;
   struct searchNode *thenode;
 
-  if (type != SEARCHTYPE_NICK) {
-    parseError = "nick: this function is only valid for nick searches.";
-    return NULL;
-  }
-
-  if (!(thenode=(struct searchNode *)malloc(sizeof (struct searchNode)))) {
+  if (!(localdata=(struct nick_localdata *)malloc(sizeof(struct nick_localdata)))) {
     parseError = "malloc: could not allocate memory for this search.";
     return NULL;
   }
+    
+  switch (type) {
+  case SEARCHTYPE_CHANNEL:
+    if (argc!=1) {
+      parseError="nick: usage: (nick target)";
+      free(localdata);
+      return NULL;
+    }
+    if ((localdata->np=getnickbynick(argv[0]))==NULL) {
+      parseError="nick: unknown nickname";
+      free(localdata);
+      return NULL;
+    }
+    localdata->type = type;
+    break;
 
-  thenode->returntype = RETURNTYPE_STRING;
-  thenode->localdata = NULL;
+  case SEARCHTYPE_NICK:
+    if (argc) {
+      parseError="nick: usage: (match (nick) target)";
+      free(localdata);
+      return NULL;
+    }
+    localdata->type = type;
+    localdata->np = NULL;
+    break;
+
+  default:
+    parseError="nick: unsupported search type";
+    free(localdata);
+    return NULL;
+  }
+
+  if (!(thenode=(struct searchNode *)malloc(sizeof(struct searchNode)))) {
+    /* couldn't malloc() memory for thenode, so free localdata to avoid leakage */
+    parseError = "malloc: could not allocate memory for this search.";
+    free(localdata);
+    return NULL;
+  }
+
+  if (type == SEARCHTYPE_CHANNEL)
+    thenode->returntype = RETURNTYPE_BOOL;
+  else
+    thenode->returntype = RETURNTYPE_STRING;
+  thenode->localdata = localdata;
   thenode->exe = nick_exe;
   thenode->free = nick_free;
 
@@ -32,16 +74,36 @@ struct searchNode *nick_parse(int type, int argc, char **argv) {
 }
 
 void *nick_exe(struct searchNode *thenode, int type, void *theinput) {
-  nick *np = (nick *)theinput;
+  struct nick_localdata *localdata;
+  nick *np;
+  chanindex *cip;
 
-  if (type != RETURNTYPE_STRING) {
-    return (void *)1;
+  localdata = thenode->localdata;
+
+  switch (localdata->type) {
+  case SEARCHTYPE_CHANNEL:
+    cip = (chanindex *)theinput;
+
+    if (cip->channel==NULL || getnumerichandlefromchanhash(cip->channel->users, localdata->np->numeric)==NULL)
+      return falseval(type); /* user/channel not found */
+    return trueval(type); /* found user on channel */
+
+  case SEARCHTYPE_NICK:
+    np = (nick *)theinput;
+
+    if (type != RETURNTYPE_STRING)
+      return (void *)1;
+    return np->nick;
+
+  default:
+    if (type != RETURNTYPE_STRING)
+      return (void *)1;
+    return "";
   }
-
-  return np->nick;
 }
 
 void nick_free(struct searchNode *thenode) {
+  free(thenode->localdata);
   free(thenode);
 }
 
