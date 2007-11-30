d1306c7a405d0cfae8453ddb220dac7dc77eb689
Chris Porter <slug@quakenet.org>
2007-11-30 18:35:30 +0000

    Add country name lookup to newsearch.

--- newsearch/ns-country.c
+++ newsearch/ns-country.c
@@ -9,6 +9,8 @@
 
 #include "../irc/irc_config.h"
 #include "../lib/irc_string.h"
+#include "../geoip/geoip.h"
+#include "../core/modules.h"
 
 void *country_exe(struct searchNode *thenode, void *theinput);
 void country_free(struct searchNode *thenode);
@@ -17,9 +19,16 @@ int ext;
 
 struct searchNode *country_parse(int type, int argc, char **argv) {
   struct searchNode *thenode;
+  GeoIP_LookupCode l;
+  int target;
 
   if (type != SEARCHTYPE_NICK) {
     parseError = "country: this function is only valid for nick searches.";
+    return NULL;
+  }
+
+  if (argc<1) {
+    parseError = "country: usage: country <country 2 character ISO code>";
     return NULL;
   }
 
@@ -29,14 +38,20 @@ struct searchNode *country_parse(int type, int argc, char **argv) {
     return NULL;
   }
   
+  l = ndlsym("geoip", "geoip_lookupcode");
+  target = l(argv[0]);
+  if(target == -1) {
+    parseError = "country: invalid country.";
+    return NULL;
+  }
 
   if (!(thenode=(struct searchNode *)malloc(sizeof (struct searchNode)))) {
     parseError = "malloc: could not allocate memory for this search.";
     return NULL;
   }
 
-  thenode->returntype = RETURNTYPE_INT;
-  thenode->localdata = NULL;
+  thenode->returntype = RETURNTYPE_BOOL;
+  thenode->localdata = (void *)target;
   thenode->exe = country_exe;
   thenode->free = country_free;
 
@@ -45,8 +60,12 @@ struct searchNode *country_parse(int type, int argc, char **argv) {
 
 void *country_exe(struct searchNode *thenode, void *theinput) {
   nick *np = (nick *)theinput;
+  int country = (int)thenode->localdata, rc = (int)np->exts[ext];
 
-  return (void *)np->exts[ext];
+  if(country == rc)
+    return (void *)1;
+
+  return (void *)0;
 }
 
 void country_free(struct searchNode *thenode) {
