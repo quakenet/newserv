16fc8bc14719d7f309c3d334ef5c03fe31e11504
Chris Porter <slug@quakenet.org>
2008-03-16 21:37:08 +0000

    Redo BUILDID stuff for pmake.

--- lib/version.h
+++ lib/version.h
@@ -3,9 +3,11 @@
 #define __VERSION_H
 
 #ifndef BUILDID
-#define BUILDID "unknown"
+#define _BUILDID "unknown"
+#else
+#define _BUILDID "BUILDID"
 #endif
 
-#define MODULE_VERSION(id) const char *_version(void) { return (id[0]=='\0')?BUILDID:(id "/" BUILDID); };
+#define MODULE_VERSION(id) const char *_version(void) { return (id[0]=='\0')?_BUILDID:(id "/" _BUILDID); };
 
 #endif
