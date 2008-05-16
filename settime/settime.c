a9f65ad0b015bed0e27e2d04a385fef09cc18d43
Paul <paul@quakenet.org>
2008-05-16 18:21:16 +0100

    allow forcing of settime commands (for devs only)

--- settime/settime.c
+++ settime/settime.c
@@ -4,8 +4,10 @@
 #include <string.h>
 #include "../irc/irc.h"
 #include "../core/schedule.h"
+#include "../control/control.h"
 
 schedule *settime_schedule;
+int settime_cmd(void *sender, int cargc, char **cargv);
 
 void settime_sendsettime()
 {
@@ -19,9 +21,19 @@ time_t newtime;
 void _init()
 {
     settime_schedule = schedulerecurring(time(NULL) + 300, 0, 1800, &settime_sendsettime, NULL);
+    registercontrolhelpcmd("settime",NO_DEVELOPER,0,&settime_cmd,"Usage: settime\nForce send a settime to network.");
+
 }
 
 void _fini()
 {
     deleteschedule(settime_schedule, &settime_sendsettime, NULL);
+    deregistercontrolcmd("settime",&settime_cmd);
+}
+
+int settime_cmd(void *sender, int cargc, char **cargv) {
+   controlreply(sender,"Sending Settime...");
+   settime_sendsettime(); 
+   controlreply(sender,"Done");
+   return CMD_OK;
 }
