3128667f3b19bef3b403f75cb509d2e7aab2cc9e
Paul <paul@quakenet.org>
2008-08-13 15:45:20 +0100

    Sample node search module.
    at the moment, only 1 output format is implemented and 2 commands - users/nick
    [15:43:41] <mmmmmm> nodesearch (eq (users) 1)
    [15:43:41] <NP> 127.0.1.3 [1]
    [15:43:41] <NP> 127.0.1.4 [1]
    [15:43:41] <NP> 127.0.1.5 [1]
    [15:43:41] <NP> --- End of list: 3 matches
    [15:43:54] <mmmmmm> nodesearch -s 127/8 (eq (users) 1)
    [15:43:54] <NP> 127.0.1.3 [1]
    [15:43:54] <NP> 127.0.1.4 [1]
    [15:43:54] <NP> 127.0.1.5 [1]
    [15:43:54] <NP> --- End of list: 3 matches
    
    Nick is fairly pointless and should probably be something to use against match etc
    
    [15:43:16] <mmmmmm> nodesearch -s 127/0 (nick mmmmmm)
    [15:43:16] <NP> 192.168.0.48 [1]
    [15:43:16] <NP> --- End of list: 1 matches
    [15:43:20] <mmmmmm> nodesearch -s 127/24 (nick mmmmmm)
    [15:43:20] <NP> --- End of list: 0 matches

---
+++ patriciasearch/ps-nick.c
@@ -0,0 +1,72 @@
+#include "patriciasearch.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *ps_nick_exe(searchCtx *ctx, struct searchNode *thenode, void *theinput);
+void ps_nick_free(searchCtx *ctx, struct searchNode *thenode);
+
+struct nick_localdata {
+  nick *np;
+};
+
+struct searchNode *ps_nick_parse(searchCtx *ctx, int argc, char **argv) {
+  struct searchNode *thenode;
+  struct nick_localdata *localdata;
+
+  if (!(localdata=(struct nick_localdata *)malloc(sizeof(struct nick_localdata)))) {
+    parseError = "malloc: could not allocate memory for this search.";
+    return NULL;
+  }
+
+  if (argc!=1) {
+    parseError="nick: usage: (nick target)";
+    free(localdata);
+    return NULL;
+  }
+  if ((localdata->np=getnickbynick(argv[0]))==NULL) {
+    parseError="nick: unknown nickname";
+    free(localdata);
+    return NULL;
+  }
+
+  if (!(thenode=(struct searchNode *)malloc(sizeof (struct searchNode)))) {
+    parseError = "malloc: could not allocate memory for this search.";
+    return NULL;
+  }
+
+  thenode->returntype = RETURNTYPE_BOOL;
+  thenode->localdata = localdata;
+  thenode->exe = ps_nick_exe;
+  thenode->free = ps_nick_free;
+
+  return thenode;
+}
+
+void *ps_nick_exe(searchCtx *ctx, struct searchNode *thenode, void *theinput) {
+  struct nick_localdata *localdata;
+  patricia_node_t *node;
+  nick *np;
+  patricianick_t *pnp;
+  patricia_node_t *pn;
+  int i;
+
+  localdata = thenode->localdata;
+  node = (patricia_node_t *)theinput;
+
+  if (pnode_ext && node->exts[pnode_ext] ) {
+    pnp = node->exts[pnode_ext];
+    for (i = 0; i < PATRICIANICK_HASHSIZE; i++) {
+      for (np = pnp->identhash[i]; np; np=np->exts[pnick_ext]) {
+        if (np == localdata->np)
+          return (void *)1;
+      }
+    }
+  }
+  return (void *)0;
+}
+
+void ps_nick_free(searchCtx *ctx, struct searchNode *thenode) {
+  free(thenode->localdata);
+  free(thenode);
+}
---
+++ patriciasearch/ps-users.c
@@ -0,0 +1,33 @@
+#include "patriciasearch.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *ps_users_exe(searchCtx *ctx, struct searchNode *thenode, void *theinput);
+void ps_users_free(searchCtx *ctx, struct searchNode *thenode);
+
+struct searchNode *ps_users_parse(searchCtx *ctx, int argc, char **argv) {
+  struct searchNode *thenode;
+
+  if (!(thenode=(struct searchNode *)malloc(sizeof (struct searchNode)))) {
+    parseError = "malloc: could not allocate memory for this search.";
+    return NULL;
+  }
+
+  thenode->returntype = RETURNTYPE_INT;
+  thenode->localdata = NULL;
+  thenode->exe = ps_users_exe;
+  thenode->free = ps_users_free;
+
+  return thenode;
+}
+
+void *ps_users_exe(searchCtx *ctx, struct searchNode *thenode, void *theinput) {
+  patricia_node_t *pn = (patricia_node_t *)theinput;
+
+  return (void *)(pn->usercount);
+}
+
+void ps_users_free(searchCtx *ctx, struct searchNode *thenode) {
+  free(thenode);
+}
