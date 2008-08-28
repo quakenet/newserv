371e8b02bba7f176625cffe8170366689771912e
Chris Porter <slug@quakenet.org>
2008-08-28 22:06:30 +0100

    ns-hostmask now makes more sense (realname and realhost options).

--- newsearch/ns-hostmask.c
+++ newsearch/ns-hostmask.c
@@ -11,11 +11,25 @@
 #include "../lib/irc_string.h"
 
 void *hostmask_exe(searchCtx *ctx, struct searchNode *thenode, void *theinput);
-void *hostmask_exe_real(searchCtx *ctx, struct searchNode *thenode, void *theinput);
+void *hostmask_exe_rn(searchCtx *ctx, struct searchNode *thenode, void *theinput);
+void *hostmask_exe_rh(searchCtx *ctx, struct searchNode *thenode, void *theinput);
+void *hostmask_exe_rn_rh(searchCtx *ctx, struct searchNode *thenode, void *theinput);
 void hostmask_free(searchCtx *ctx, struct searchNode *thenode);
 
 struct searchNode *hostmask_parse(searchCtx *ctx, int argc, char **argv) {
   struct searchNode *thenode;
+  int realhost = 0, realname = 0, i;
+  
+  for(i=0;i<argc;i++) {
+    if(!ircd_strcmp(argv[i], "realhost")) {
+      realhost = 1;
+    } else if(!ircd_strcmp(argv[i], "realname")) {
+      realname = 1;
+    } else {
+      parseError = "bad argument: use realhost and/or realname";
+      return NULL;
+    }
+  }
 
   if (!(thenode=(struct searchNode *)malloc(sizeof (struct searchNode)))) {
     parseError = "malloc: could not allocate memory for this search.";
@@ -29,13 +43,20 @@ struct searchNode *hostmask_parse(searchCtx *ctx, int argc, char **argv) {
     free(thenode);
     return NULL;
   }
-  thenode->exe = hostmask_exe;
   thenode->free = hostmask_free;
 
-  /* Allow "hostmask real" to match realhost */
-
-  if (argc>0 && !ircd_strcmp(argv[0],"real")) {
-    thenode->exe = hostmask_exe_real;
+  if(realname) {
+    if(realhost) {
+      thenode->exe = hostmask_exe_rn_rh;
+    } else {
+      thenode->exe = hostmask_exe_rn;
+    }
+  } else {
+    if(realhost) {
+      thenode->exe = hostmask_exe_rh;
+    } else {
+      thenode->exe = hostmask_exe;
+    }
   }
 
   return thenode;
@@ -48,11 +69,29 @@ void *hostmask_exe(searchCtx *ctx, struct searchNode *thenode, void *theinput) {
   return visiblehostmask(np, buf);
 }
 
-void *hostmask_exe_real(searchCtx *ctx, struct searchNode *thenode, void *theinput) {
+void *hostmask_exe_rh(searchCtx *ctx, struct searchNode *thenode, void *theinput) {
+  nick *np = (nick *)theinput;
+  char *buf = thenode->localdata;
+
+  sprintf(buf,"%s!%s@%s",np->nick,np->ident,np->host->name->content);
+
+  return buf;
+}
+
+void *hostmask_exe_rn_rh(searchCtx *ctx, struct searchNode *thenode, void *theinput) {
   nick *np = (nick *)theinput;
   char *buf = thenode->localdata;
 
   sprintf(buf,"%s!%s@%s\r%s",np->nick,np->ident,np->host->name->content,np->realname->name->content);
+
+  return buf;
+}
+
+void *hostmask_exe_rn(searchCtx *ctx, struct searchNode *thenode, void *theinput) {
+  nick *np = (nick *)theinput;
+  char *buf = thenode->localdata;
+
+  sprintf(buf,"%s\r%s",visiblehostmask(np, buf),np->realname->name->content);
 
   return buf;
 }
