f2a9a545b0637368e1f9f7a5768e3312c99ed293
Chris Porter <slug@quakenet.org>
2008-09-11 02:10:22 +0100

    Typo fixes, also forgot to hg add my sqlite wrapper.

--- regexgline/regexgline.c
+++ regexgline/regexgline.c
@@ -153,7 +153,7 @@ void rg_checkexpiry(void *arg) {
   
   while(rp) {
     if (current >= rp->expires) {
-      rg_sqlquery("DELETE FROM regexglines WHERE id = %d", rp->id);
+      dbquery("DELETE FROM regexglines WHERE id = %d", rp->id);
       if (lp) {
         lp->next = rp->next;
         rg_freestruct(rp);
---
+++ sqlite/libsqlite3.c
@@ -0,0 +1,3 @@
+#define SQLITE_THREADSAFE 0
+
+#include "libsqlite3/sqlite3.c"
