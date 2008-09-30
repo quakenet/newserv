c2b5c1de5c755e5ff6fa7e05ea5b463fc896d47a
Chris Porter <slug@quakenet.org>
2008-09-30 05:47:08 +0100

    Add trusts_migration module.

---
+++ trusts_migration/trusts_migration.h
@@ -0,0 +1,27 @@
+#ifndef __TRUSTS_H
+#define __TRUSTS_H
+
+#include "../dbapi2/dbapi2.h"
+#include <time.h>
+
+extern DBAPIConn *tsdb;
+
+struct trustmigration;
+
+typedef void (*TrustMigrationGroup)(struct trustmigration *, unsigned int, char *, unsigned int, unsigned int, unsigned int, unsigned int, time_t, time_t, time_t, char *, char *, char *);
+typedef void (*TrustMigrationHost)(struct trustmigration *, unsigned int, char *, unsigned int, time_t);
+typedef void (*TrustMigrationFini)(struct trustmigration *, int);
+
+typedef struct trustmigration {
+  int count, cur;
+  void *schedule;
+
+  TrustMigrationGroup group;
+  TrustMigrationHost host;
+  TrustMigrationFini fini;
+} trustmigration;
+
+trustmigration *trusts_migration_start(TrustMigrationGroup, TrustMigrationHost, TrustMigrationFini);
+void trusts_migration_stop(trustmigration *);
+
+#endif
