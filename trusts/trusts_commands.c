05cae8fd466935bac3080315a25e2ad54330d4a9
Chris Porter <slug@quakenet.org>
2008-10-03 23:19:44 +0100

    Move migration out of _commands.

--- trusts/trusts_commands.c
+++ trusts/trusts_commands.c
@@ -2,51 +2,8 @@
 #include "../lib/irc_string.h"
 #include "trusts.h"
 
-int trusts_migration_start(TrustDBMigrationCallback, void *);
-void trusts_migration_stop(void);
 static void registercommands(int, void *);
 static void deregistercommands(int, void *);
-
-static void migrate_status(int errcode, void *tag) {
-  long sender = (long)tag;
-  nick *np = getnickbynumeric(sender);
-
-  if(!np)
-    return;
-
-  if(!errcode || errcode == MIGRATION_LASTERROR) {
-    if(!errcode) {
-       controlreply(np, "Migration complete.");
-       controlreply(np, "Attempting to reload database. . .");
-    } else {
-      controlreply(np, "An error occured after the database was unloaded, attempting reload. . .");
-    }
-    if(trusts_loaddb()) {
-      controlreply(np, "Database reloaded successfully.");
-    } else {
-      controlreply(np, "An error occured, please reload the module manually.");
-    }
-  } else {
-    controlreply(np, "Error %d occured during migration, commands reregistered.", errcode);
-    registercommands(0, NULL);
-  }
-}
-
-static int trusts_cmdmigrate(void *source, int cargc, char **cargv) {
-  nick *sender = source;
-  int ret;
-
-  /* iffy but temporary */
-  ret = trusts_migration_start(migrate_status, (void *)(sender->numeric));
-  if(!ret) {
-    controlreply(sender, "Migration started, commands deregistered.");
-    deregistercommands(0, NULL);
-  } else {
-    controlreply(sender, "Error %d starting migration.", ret);
-  }
-
-  return CMD_OK;
-}
 
 static int trusts_cmdtrustlist(void *source, int cargc, char **cargv) {
   nick *sender = source;
@@ -95,7 +52,6 @@ static void registercommands(int hooknum, void *arg) {
     return;
   commandsregistered = 1;
 
-  registercontrolhelpcmd("trustmigrate", NO_DEVELOPER, 0, trusts_cmdmigrate, "Usage: trustmigrate\nCopies trust data from O and reloads the database.");
   registercontrolhelpcmd("trustlist", NO_OPER, 1, trusts_cmdtrustlist, "Usage: trustlist <#id|name|id>\nShows trust data for the specified trust group.");
 }
 
@@ -104,7 +60,6 @@ static void deregistercommands(int hooknum, void *arg) {
     return;
   commandsregistered = 0;
 
-  deregistercontrolcmd("trustmigrate", trusts_cmdmigrate);
   deregistercontrolcmd("trustlist", trusts_cmdtrustlist);
 }
 
@@ -119,8 +74,6 @@ void _init(void) {
 void _fini(void) {
   deregisterhook(HOOK_TRUSTS_DB_LOADED, registercommands);
   deregisterhook(HOOK_TRUSTS_DB_CLOSED, deregistercommands);
-
-  trusts_migration_stop();
 
   deregistercommands(0, NULL);
 }
