c6fc47f17737b0bb0d91d5d6347cd742a18ddb50
Chris Porter <slug@quakenet.org>
2010-03-28 00:57:51 +0000

    LUALIB: allow achievement send to take longs.

--- lua/lib/quakenet/achievements.lua
+++ lua/lib/quakenet/achievements.lua
@@ -5,17 +5,27 @@ Achievements = class(function(obj, bot_id, types)
   obj.types = types
 end)
 
-function Achievements:send(nick, type, ...)
+function Achievements:send(nick, achievement, ...)
   local extra = ...
   if not extra then
     extra = 0
   end
 
-  local ntype = self.types[type]
+  local ntype = self.types[achievement]
   if ntype then
-    type = ntype
+    achievement = ntype
   end
 
-  local user_numeric = irc_numerictobase64(nick.numeric)
-  irc_privmsg("C", "EXTACH " .. user_numeric .. " " .. self.bot_id .. " " .. type .. " " .. extra)
+  local user_numeric
+  if type(nick) == "table" then
+    user_numeric = irc_numerictobase64(nick.numeric)
+  else -- assume it's a numeric
+    user_numeric = irc_numerictobase64(nick)
+  end
+  
+  if not user_numeric then
+    return
+  end
+
+  irc_privmsg("C", "EXTACH " .. user_numeric .. " " .. self.bot_id .. " " .. achievement .. " " .. extra)
 end
