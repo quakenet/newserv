1d5b35a818bda81bee6f434d90f4cf144ee0a18a
Chris Porter <slug@quakenet.org>
2010-11-23 01:34:17 +0000

    CHANSERV: allow .mil email addresses.
    --HG--
    branch : achievements

--- chanserv/chanserv.h
+++ chanserv/chanserv.h
@@ -241,7 +241,7 @@
 
 /* email */
 #define MAX_RESEND_TIME      2*3600L  /* cooling off period */
-#define VALID_EMAIL         "^[-_.+[:alpha:][:digit:]]+(\\.[-_[:digit:][:alpha:]]+)*@([[:digit:][:alpha:]](-?[[:digit:][:alpha:]])*\\.)+[[:alpha:]]{2}([zmuvtgo]|fo|me|seum|op|ro)?$"
+#define VALID_EMAIL         "^[-_.+[:alpha:][:digit:]]+(\\.[-_[:digit:][:alpha:]]+)*@([[:digit:][:alpha:]](-?[[:digit:][:alpha:]])*\\.)+[[:alpha:]]{2}([zmuvtgol]|fo|me|seum|op|ro)?$"
 
 #define VALID_ACCOUNT_NAME  "^[-a-z0-9]*$"
 
