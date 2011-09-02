9958fec939b733c13c137a50e3a97ef01f0bb93f
Chris Porter <slug@quakenet.org>
2011-09-02 22:44:13 +0100

    FAKEQ: initial import

---
+++ fakeq/fakeq.c
@@ -0,0 +1,17 @@
+#include "../chanserv/chanserv.h"
+
+static nick *fakeq;
+
+void chanservuserhandler(nick *target, int message, void **params);
+
+void _init(void) {
+  fakeq=registerlocaluser("Q","TheQBot","CServe.quakenet.org",
+                                 "The Q Bot","Q",
+                                 UMODE_INV|UMODE_SERVICE|UMODE_DEAF|UMODE_OPER|UMODE_ACCOUNT,
+                                 &chanservuserhandler);  
+}
+
+void _fini(void) {
+  if(fakeq)
+    deregisterlocaluser(fakeq, "Leaving");
+}
