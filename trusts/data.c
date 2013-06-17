373458b6019b6b95f6e075c128e5a3b9ae86c650
Gunnar Beutner <shroud@quakenet.org>
2013-06-17 11:27:55 +0200

    Fix crash with empty TGs.
    --HG--
    branch : shroudtrusts

--- trusts/data.c
+++ trusts/data.c
@@ -237,7 +237,7 @@ static trusthost *th_getnextchildbyhost(trusthost *orig, trusthost *th) {
       th = th->group->next->hosts;
     }
 
-    if(th->parent == orig)
+    if(!th || th->parent == orig)
       return th;
   }
 }
