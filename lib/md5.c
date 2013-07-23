b07100835b529c30f5231edb2c4b1f1798a6d7bf
Gunnar Beutner <shroud@quakenet.org>
2013-07-23 19:23:28 +0200

    lib/md5.c: Fix incorrect sizeof().

--- lib/md5.c
+++ lib/md5.c
@@ -174,7 +174,7 @@ MD5Final (digest, ctx)
 	putu32(ctx->buf[1], digest + 4);
 	putu32(ctx->buf[2], digest + 8);
 	putu32(ctx->buf[3], digest + 12);
-	memset(ctx, 0, sizeof(ctx));	/* In case it's sensitive */
+	memset(ctx, 0, sizeof(MD5Context));	/* In case it's sensitive */
 }
 
 /* The four core functions - F1 is optimized somewhat */
