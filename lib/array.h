6d6ee81d80201030b672156b6bd76f22959b3024
Chris Porter <slug@quakenet.org>
2020-07-05 19:17:44 +0100

    CONTROL: increase command array size in SHOWCOMMANDS (can't see >command 100)

--- lib/array.h
+++ lib/array.h
@@ -21,4 +21,6 @@ void array_delslot(array *a, unsigned int index);
 #define array_setlim1(a, size) (a)->allocchunksize = size;
 #define array_setlim2(a, size) (a)->freechunksize = size;
 
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+
 #endif /* _ARRAY_H */
