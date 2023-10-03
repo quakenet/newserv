2f7e3cea18cffa652294824d589c67de70e83947
Chris Porter <slug@quakenet.org>
2023-10-03 21:34:02 +0100

    JUPE: cleanup code and fix UAF bugs

--- jupe/jupe.h
+++ jupe/jupe.h
@@ -7,8 +7,6 @@ typedef struct jupe_s {
 	unsigned int	ju_flags;
 } jupe_t;
 
-extern jupe_t *jupes;
-
 #define JUPE_MAX_EXPIRE	604800
 
 #define JUPE_ACTIVE	0x0001
@@ -19,13 +17,8 @@ extern jupe_t *jupes;
 #define JupeReason(j)		((j)->ju_reason->content)
 #define JupeLastMod(j)		((j)->ju_lastmod)
 
-void jupe_propagate(jupe_t *jupe);
-jupe_t *make_jupe(char *server, char *reason, time_t expirets, time_t lastmod, unsigned int flags);
-void jupe_free(jupe_t *jupe);
-
-/* (public) functions for using/modifying jupes */
 jupe_t *jupe_find(char *server);
 void jupe_activate(jupe_t *jupe);
 void jupe_deactivate(jupe_t *jupe);
 int jupe_add(char *server, char *reason, time_t duration, unsigned int flags);
-void jupe_expire(void); /* call this before directly using the jupes list */
+jupe_t *jupe_next(jupe_t *current);
